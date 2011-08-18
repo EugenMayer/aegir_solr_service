09c0909033c22f0a4e1b18eb20fe73f53998393d
Elliot Pahl <elliot@catalyst.net.nz>
2011-08-18 22:33:25 +1200

    Add solr provision module for Aegir.

---
+++ provision/solr/apollo/apollo.tpl.php
@@ -0,0 +1,5 @@
+This is an solr template for the solr service in provision.
+
+The value set in the UI for this service was : <?php print $this->solr_field; ?>
+
+The time generated in the data_options() method was: <?php print $solr_current_time; ?>
